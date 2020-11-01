2382d6d60aed036cc5756de764e86d6a58a16632
Oleksandr Redko <oleksandr.red+github@gmail.com>
2020-11-01 20:24:55 +0200

    Refactor storage

--- internal/storage/mongo.go
+++ internal/storage/mongo.go
@@ -21,14 +21,15 @@ const (
 )
 
 type Storage struct {
-	mc *mongo.Client
-	db *mongo.Database
+	collectionArticle       *mongo.Collection
+	collectionAutoincrement *mongo.Collection
 }
 
 func New(mc *mongo.Client, database string) *Storage {
+	db := mc.Database(database)
 	return &Storage{
-		mc: mc,
-		db: mc.Database(database),
+		collectionArticle:       db.Collection(collectionArticles),
+		collectionAutoincrement: db.Collection(collectionAutoincrement),
 	}
 }
 
@@ -42,19 +43,19 @@ func (s *Storage) NextArticleID(ctx context.Context) (int, error) {
 }
 
 func (s *Storage) CreateArticle(ctx context.Context, id int, content string, duplicateIDs []int) error {
-	a := article{
+	art := article{
 		ID:           id,
 		Content:      content,
 		DuplicateIDs: duplicateIDs,
 		CreatedAt:    time.Now(),
 	}
 
-	ma, err := bson.Marshal(&a)
+	ma, err := bson.Marshal(&art)
 	if err != nil {
 		return errors.Wrap(err, "failed to marshal")
 	}
 
-	if _, err := s.db.Collection(collectionArticles).InsertOne(ctx, ma); err != nil {
+	if _, err := s.collectionArticle.InsertOne(ctx, ma); err != nil {
 		return errors.Wrap(err, "failed to insert")
 	}
 
@@ -62,7 +63,7 @@ func (s *Storage) CreateArticle(ctx context.Context, id int, content string, dup
 }
 
 func (s *Storage) ArticleByID(ctx context.Context, id int) (model.Article, error) {
-	res := s.db.Collection(collectionArticles).FindOne(ctx, bson.D{{Key: "id", Value: id}})
+	res := s.collectionArticle.FindOne(ctx, bson.D{{Key: "id", Value: id}})
 	if errors.Is(res.Err(), mongo.ErrNoDocuments) {
 		return model.Article{}, errors.Wrap(internalErrors.ErrNotFound, "not found")
 	}
@@ -76,43 +77,39 @@ func (s *Storage) ArticleByID(ctx context.Context, id int) (model.Article, error
 		return model.Article{}, errors.Wrap(err, "failed to decode")
 	}
 
-	return model.Article{
-		ID:           art.ID,
-		Content:      art.Content,
-		DuplicateIDs: art.DuplicateIDs,
-	}, nil
+	return toModelArticle(art), nil
 }
 
 func (s *Storage) AllArticles(ctx context.Context) ([]model.Article, error) {
 	articles := make([]model.Article, 0, maxArticles)
-	collection := s.db.Collection(collectionArticles)
 
-	cur, err := collection.Find(ctx, bson.D{})
+	cur, err := s.collectionArticle.Find(ctx, bson.D{})
 	if err != nil {
 		return nil, errors.Wrap(err, "failed to find all documents")
 	}
 
 	for cur.TryNext(ctx) && len(articles) != maxArticles {
-		art := &article{}
-
-		if err := cur.Decode(art); err != nil {
+		art := article{}
+		if err := cur.Decode(&art); err != nil {
 			return nil, errors.Wrap(err, "failed to cursor decode")
 		}
 
-		articles = append(articles, model.Article{
-			ID:           art.ID,
-			Content:      art.Content,
-			DuplicateIDs: art.DuplicateIDs,
-		})
+		articles = append(articles, toModelArticle(art))
 	}
 
 	return articles, nil
 }
 
+func toModelArticle(art article) model.Article {
+	return model.Article{
+		ID:           art.ID,
+		Content:      art.Content,
+		DuplicateIDs: art.DuplicateIDs,
+	}
+}
+
 func (s *Storage) autoincrement(ctx context.Context, collection string) (*autoincrement, error) {
-	opts := options.FindOneAndUpdate().
-		SetReturnDocument(options.After).
-		SetUpsert(true)
+	opts := options.FindOneAndUpdate().SetReturnDocument(options.After).SetUpsert(true)
 	doc := &autoincrement{}
 	filter := bson.M{"collection": collection}
 	update := bson.M{
@@ -120,7 +117,7 @@ func (s *Storage) autoincrement(ctx context.Context, collection string) (*autoin
 		"$set": bson.M{"updated_at": time.Now()},
 	}
 
-	if err := s.db.Collection(collectionAutoincrement).FindOneAndUpdate(ctx, filter, update, opts).
+	if err := s.collectionAutoincrement.FindOneAndUpdate(ctx, filter, update, opts).
 		Decode(&doc); err != nil {
 		return nil, errors.Wrap(err, "failed to find one and update")
 	}
